b90f5a15f5cadd9d82d6b7ea47ebc52834eadb19
dtan4 <dtanshi45@gmail.com>
2013-10-11 14:26:26 +0900

    Template#initialize call Template#set_param

--- lib/ajweb_generator/utils/template.rb
+++ lib/ajweb_generator/utils/template.rb
@@ -1,6 +1,10 @@
 module AjwebGenerator
   class Template
     def initialize
+      set_param
+    end
+
+    def set_param
 
     end
   end
--- spec/ajweb_generator/utils/template_spec.rb
+++ spec/ajweb_generator/utils/template_spec.rb
@@ -3,6 +3,13 @@ require "spec_helper"
 module AjwebGenerator
   describe Template do
     describe "#initialize" do
+      it "should call #set_param" do
+        AjwebGenerator::Template.any_instance.should_receive(:set_param)
+        AjwebGenerator::Template.new
+      end
+    end
+
+    describe "#set_param" do
       pending
     end
 
