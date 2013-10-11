388bc047c7e3a3ccaacdd582ba67400eaf2c6842
dtan4 <dtanshi45@gmail.com>
2013-10-11 14:58:16 +0900

    Refactor spec

--- spec/ajweb_generator/utils/fileutils_spec.rb
+++ spec/ajweb_generator/utils/fileutils_spec.rb
@@ -4,33 +4,50 @@ module AjwebGenerator
   describe FileUtils do
     let(:filename) { "example.txt" }
     let(:text) { "hello, world!" }
+    let(:exist_text) { "goodby, world!" }
 
     describe "#write_file" do
-      context "when overwrite is true" do
-        subject { AjwebGenerator::FileUtils.write_file(filename, text, true) }
+      context "when file is not existed" do
+        context "and overwrite is true" do
+          let(:result) { AjwebGenerator::FileUtils.write_file(filename, text, true) }
 
-        context "and file is not existed" do
-          it { should be_true }
+          it "should return true" do
+            result.should be_true
+          end
         end
 
-        context "and file is already existed" do
-          before { open(filename, "w") { |f| f.puts text} }
-          it { should be_true }
-          after { File.delete(filename) if File.exists?(filename) }
+        context "and overwrite is false" do
+          let(:result) { AjwebGenerator::FileUtils.write_file(filename, text, false) }
+
+          it "should return true" do
+            result.should be_true
+          end
         end
       end
 
-      context "when overwrite is false" do
-        subject { AjwebGenerator::FileUtils.write_file(filename, text, false) }
-
-        context "and file is not existed" do
-          it { should be_true }
+      context "when file is already existed" do
+        before do
+          open(filename, "w") { |f| f.puts exist_text }
         end
 
-        context "and file is already existed" do
-          before { open(filename, "w") { |f| f.puts text } }
-          it { should be_false }
-          after { File.delete(filename) if File.exists?(filename) }
+        context "and overwrite is true" do
+          let(:result) { AjwebGenerator::FileUtils.write_file(filename, text, true) }
+
+          it "should return true" do
+            result.should be_true
+          end
+        end
+
+        context "and overwrite is false" do
+          let(:result) { AjwebGenerator::FileUtils.write_file(filename, text, false) }
+
+          it "should return false" do
+            result.should be_false
+          end
+        end
+
+        after do
+          File.delete(filename) if File.exists?(filename)
         end
       end
     end
