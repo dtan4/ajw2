08cdae5c4744a05df09a598a0c960dc19cf61294
dtan4 <dtanshi45@gmail.com>
2013-10-11 16:01:40 +0900

    Make Application as pending

--- lib/ajweb_generator/model/application.rb
+++ lib/ajweb_generator/model/application.rb
@@ -1,7 +1,32 @@
 module AjwebGenerator
   class Application
-    def generate
+    DEFAULT_HTML_NAME = "/index.html"
+    DEFAULT_CSS_NAME = "/index.css"
+    DEFAULT_JS_NAME = "/index.js"
 
+    def generate(dirname)
+      html_generate(dirname)
+      css_generate(dirname)
+      js_generate(dirname)
+      db_generate(dirname)
+    end
+
+    def html_generate(dirname)
+      htmlname = dirname << DEFAULT_HTML_NAME
+      html = ""
+      AjwebGenerator::FileUtils.write_file(htmlname, )
+    end
+
+    def css_generate(dirname)
+      cssname = dirname << DEFAULT_CSS_NAME
+    end
+
+    def js_generate(dirname)
+      jsname = dirname << DEFAULT_JS_NAME
+    end
+
+    def db_generate(dirname)
+      # TODO
     end
   end
 end
--- spec/ajweb_generator/model/application_spec.rb
+++ spec/ajweb_generator/model/application_spec.rb
@@ -2,24 +2,72 @@ require "spec_helper"
 
 module AjwebGenerator
   describe Application do
-    describe "#generate" do
+    let(:app) { AjwebGenerator::Application.new }
+    let(:dirname) { "outdir" }
+    let(:default_html) { "/index.html" }
+    let(:default_css) { "/index.cs" }
+    let(:default_js) { "/index.js" }
+
+    before(:all) do
       pending
+    end
+
+    describe "#generate" do
+      it "should call Application#html_generate" do
+        app.should_receive(:html_generate)
+        app.generate(dirname)
+      end
+
+      it "should call Application#js_generate" do
+        app.should_receive(:js_generate)
+        app.generate(dirname)
+      end
+
+      it "should call Application#css_generate" do
+        app.should_receive(:css_generate)
+        app.generate(dirname)
+      end
+
+      it "should call Application#db_generate" do
+        app.should_receive(:db_generate)
+        app.generate(dirname)
+      end
     end
 
     describe "#html_generate" do
-      pending
+      it "should call FileUtils#write_file" do
+        AjwebGenerator::FileUtils.should_receive(:write_file)
+        app.html_generate(dirname)
+      end
     end
 
     describe "#css_generate" do
-      pending
+      it "should call FileUtils#write_file" do
+        AjwebGenerator::FileUtils.should_receive(:write_file)
+        app.css_generate(dirname)
+      end
     end
 
     describe "#js_generate" do
-      pending
+      it "should call FileUtils#write_file" do
+        AjwebGenerator::FileUtils.should_receive(:write_file)
+        app.js_generate(dirname)
+      end
     end
 
     describe "#db_generate" do
-      pending
+      it "should call FileUtils#write_file" do
+        AjwebGenerator::FileUtils.should_receive(:write_file)
+        app.db_generate(dirname)
+      end
+    end
+
+    after(:each) do
+      [default_html, default_css, default_js].map do |nm|
+        dirname << nm
+      end.each do |name|
+        File.delete(name) if File.exists?(name)
+      end
     end
   end
 end
