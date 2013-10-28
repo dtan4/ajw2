40add2184fd6a5ac19343497988292c60814516d
dtan4 <dtanshi45@gmail.com>
2013-10-28 16:19:08 +0900

    Delete ajweb_generator/utils/template

--- lib/ajweb_generator.rb
+++ lib/ajweb_generator.rb
@@ -1,7 +1,6 @@
 require "ajweb_generator/version"
 require "ajweb_generator/generator/compiler"
 require "ajweb_generator/model/application"
-require "ajweb_generator/utils/template"
 require "ajweb_generator/utils/fileutils"
 
 module AjwebGenerator
