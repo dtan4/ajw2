0bce3ab02f4ade1e70284690110663f7a9a90ff7
dtan4 <dtanshi45@gmail.com>
2013-10-30 18:13:02 +0900

    Add argument child_elements to #input

--- lib/ajw2/template/element.rb
+++ lib/ajw2/template/element.rb
@@ -4,7 +4,7 @@ module Ajw2::Template
   class Element
     INPUT_TYPE = %w{hidden text search url telephone email password datetime date month week time datetime-local number range color checkbox radio file submit image reset button}
 
-    def input(options)
+    def input(options, child_elements = {})
       raise Exception unless options[:type] && INPUT_TYPE.include?(options[:type])
 
       options = escape_options(options)
