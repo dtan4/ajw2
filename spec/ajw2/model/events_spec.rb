39d4037fc6b2ebb861d3cb2a179b082681c4f230
dtan4 <dtanshi45@gmail.com>
2013-11-17 16:45:34 +0900

    Fix sample sources

--- spec/ajw2/model/events_spec.rb
+++ spec/ajw2/model/events_spec.rb
@@ -3,59 +3,57 @@ require "spec_helper"
 module Ajw2::Model
   describe Events do
     let(:source) {
-      {
-        event: [
-                {
-                  target: "button1",
-                  type: :onClick,
-                  action: {
-                    login: {
-                      id: "login0",
+      [
+       {
+         target: "button1",
+         type: :onClick,
+         action: {
+           login: {
+             id: "login0",
+             param: [
+                     { id: "param1",
+                       name: "user_id",
+                       type: :string,
+                       value: {
+                         element: "userIdTextbox",
+                         func: :getValue,
+                         type: :element,
+                         elemType: :widget
+                       }
+                     }
+                    ]
+           },
+           branch: { id: "branch1", condition: { operator: :success }  },
+         then: {
+             id: "then1",
+             call: [
+                    {
+                      element: "rootFrame",
+                      func: :selectPanel,
+                      id: "call2",
                       param: [
-                              { id: "param1",
-                                name: "user_id",
-                                type: :string,
-                                value: {
-                                  element: "userIdTextbox",
-                                  func: :getValue,
-                                  type: :element,
-                                  elemType: :widget
+                              {
+                                id: "param13",
+                                name: "panel",
+                                type: :element,
+                                element: {
+                                  target: "roomSelectPanel",
+                                  type: :child
                                 }
                               }
                              ]
-                    },
-                    branch: { id: "branch1", condition: { operator: :success }  },
-                  then: {
-                      id: "then1",
-                      call: [
-                             {
-                               element: "rootFrame",
-                               func: :selectPanel,
-                               id: "call2",
-                               param: [
-                                       {
-                                         id: "param13",
-                                         name: "panel",
-                                         type: :element,
-                                         element: {
-                                           target: "roomSelectPanel",
-                                           type: :child
-                                         }
-                                       }
-                                      ]
-                             }
-                            ]
-                    },
-                  else: { id: "else1" }
-                  }
-                }
-               ]
-      }
+                    }
+                   ]
+           },
+         else: { id: "else1" }
+         }
+       }
+      ]
     }
 
     describe "#initialize" do
       subject { Ajw2::Model::Events.new(source) }
-      its(:source) { should be_instance_of Hash }
+      its(:source) { should be_instance_of Array }
     end
   end
 end
--- spec/ajw2/model/interfaces_spec.rb
+++ spec/ajw2/model/interfaces_spec.rb
@@ -3,33 +3,38 @@ require "spec_helper"
 module Ajw2::Model
   describe Interfaces do
     let(:source) {
-      {
-        panel: {
-          _isDisplay: true,
-          height: 500,
-          id: "rootPanel",
-          left: 72,
-          top: 2,
-          width: 700,
-          label: {
-            content: "Chat Application",
-            id: "label0",
-            left: 27,
-            top: 22
-          },
-          button: {
-            content: "Selection",
-            id: "selectButton",
-            left: 369,
-            top: 50
-          }
-        }
-      }
+      [
+       {
+         type: :panel,
+         _isDisplay: true,
+         height: 500,
+         id: "rootPanel",
+         left: 72,
+         top: 2,
+         width: 700,
+         children: [
+                    {
+                      type: :label,
+                      content: "Chat Application",
+                      id: "label0",
+                      left: 27,
+                      top: 22
+                    },
+                    {
+                      type: :button,
+                      content: "Selection",
+                      id: "selectButton",
+                      left: 369,
+                      top: 50
+                    }
+                   ]
+       }
+      ]
     }
 
     describe "#initialize" do
       subject { Ajw2::Model::Interfaces.new(source) }
-      its(:source) { should be_instance_of Hash }
+      its(:source) { should be_instance_of Array }
     end
   end
 end
