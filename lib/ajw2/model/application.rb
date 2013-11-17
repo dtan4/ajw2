12b7b2c0caceb6d436ae8e5535edb4806d28cbb2
dtan4 <dtanshi45@gmail.com>
2013-11-17 15:50:09 +0900

    Rename Ajw2::Application to Ajw2::Model::Application

--- lib/ajw2/model/application.rb
+++ lib/ajw2/model/application.rb
@@ -1,31 +1,9 @@
-module Ajw2
+module Ajw2::Model
   class Application
-    DEFAULT_HTML_NAME = "/index.html"
-    DEFAULT_CSS_NAME = "/index.css"
-    DEFAULT_JS_NAME = "/index.js"
+    attr_reader :name
 
-    def generate(dirname)
-      html_generate(dirname)
-      css_generate(dirname)
-      js_generate(dirname)
-      db_generate(dirname)
-    end
-
-    def html_generate(dirname)
-      htmlname = dirname << DEFAULT_HTML_NAME
-      html = ""
-    end
-
-    def css_generate(dirname)
-      cssname = dirname << DEFAULT_CSS_NAME
-    end
-
-    def js_generate(dirname)
-      jsname = dirname << DEFAULT_JS_NAME
-    end
-
-    def db_generate(dirname)
-      # TODO
+    def initialize(name)
+      @name = name
     end
   end
 end
--- spec/ajw2/model/application_spec.rb
+++ spec/ajw2/model/application_spec.rb
@@ -1,57 +1,10 @@
 require "spec_helper"
 
-module Ajw2
+module Ajw2::Model
   describe Application do
-    let(:app) { Ajw2::Application.new }
-    let(:dirname) { "outdir" }
-    let(:default_html) { "/index.html" }
-    let(:default_css) { "/index.css" }
-    let(:default_js) { "/index.js" }
-
-    before(:all) do
-      pending
-    end
-
-    describe "#generate" do
-      it "should call Application#html_generate" do
-        app.should_receive(:html_generate)
-        app.generate(dirname)
-      end
-
-      it "should call Application#js_generate" do
-        app.should_receive(:js_generate)
-        app.generate(dirname)
-      end
-
-      it "should call Application#css_generate" do
-        app.should_receive(:css_generate)
-        app.generate(dirname)
-      end
-
-      it "should call Application#db_generate" do
-        app.should_receive(:db_generate)
-        app.generate(dirname)
-      end
-    end
-
-    describe "#html_generate" do
-    end
-
-    describe "#css_generate" do
-    end
-
-    describe "#js_generate" do
-    end
-
-    describe "#db_generate" do
-    end
-
-    after(:each) do
-      [default_html, default_css, default_js].map do |nm|
-        dirname << nm
-      end.each do |name|
-        File.delete(name) if File.exists?(name)
-      end
+    describe "#initialize" do
+      subject { Ajw2::Model::Application.new("sample") }
+      its(:name) { should be_instance_of String }
     end
   end
 end
