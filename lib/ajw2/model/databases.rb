a82a288434a2a33c1277df071dbf23052fcd76fc
dtan4 <dtanshi45@gmail.com>
2013-11-22 16:33:19 +0900

    Add Databases#render_schema_*

--- lib/ajw2/model/databases.rb
+++ lib/ajw2/model/databases.rb
@@ -5,5 +5,39 @@ module Ajw2::Model
     def initialize(source)
       @source = source
     end
+
+    def render_schema
+      @source[:database].inject([]) { |schema, table| schema << render_table_schema(table) }
+    end
+
+    private
+    def render_table_schema(table)
+      schema = ["create_table \"#{table[:tablename]}\", force: true do |t|"]
+      schema.concat render_schema_fields(table)
+      schema << "end"
+      schema.join("\n") + "\n"
+    end
+
+    def render_schema_fields(table)
+      table[:property].inject([]) do |fields, field|
+        fields << render_schema_field(field)
+      end.concat [
+                  "  t.datetime \"created_at\", null: false",
+                  "  t.datetime \"updated_at\", null: false"
+                 ]
+    end
+
+    def render_schema_field(field)
+        line = case field[:type]
+               when :password
+                 "  t.string \"crypted_#{field[:name]}\""
+               when :role # TODO role validation
+                 "  t.string \"#{field[:name]}\""
+               else
+                 "  t.#{field[:type]} \"#{field[:name]}\""
+               end
+        line << ", null: false" unless field[:null].nil? || field[:null]
+        line
+    end
   end
 end
--- spec/ajw2/model/databases_spec.rb
+++ spec/ajw2/model/databases_spec.rb
@@ -12,9 +12,17 @@ module Ajw2::Model
            tablename: "users",
            type: :server,
            property: [
-                      { name: "user_id", type: :string, unique: true },
-                      { name: "password", type: :password, unique: false },
-                      { name: "role", type: :role, unique: false }
+                      { name: "username", type: :string, unique: true, null: false },
+                      { name: "password", type: :password },
+                      { name: "role", type: :role  }
+                     ]
+         },
+         {
+           tablename: "messages",
+           type: :server,
+           property: [
+                      { name: "user_id", type: :integer, unique: false, null:false },
+                      { name: "content", type: :string, unique: false, null: false }
                      ]
          }
         ]
@@ -24,6 +32,41 @@ module Ajw2::Model
     describe "#initialize" do
       subject { Ajw2::Model::Databases.new(source) }
       its(:source) { should be_instance_of Hash }
+    end
+
+    describe "#render_schema" do
+      before { @schema = Ajw2::Model::Databases.new(source).render_schema }
+
+      it "should return Array" do
+        expect(@schema).to be_an_instance_of Array
+      end
+
+      it "should contain 2 items" do
+        expect(@schema.length).to eq 2
+      end
+
+      it "should render the schema of users table" do
+        expect(@schema[0]).to eq(<<-EOS)
+create_table "users", force: true do |t|
+  t.string "username", null: false
+  t.string "crypted_password"
+  t.string "role"
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
+                              EOS
+      end
+
+      it "should render the schema of messages table" do
+        expect(@schema[1]).to eq(<<-EOS)
+create_table "messages", force: true do |t|
+  t.integer "user_id", null: false
+  t.string "content", null: false
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
+                              EOS
+      end
     end
   end
 end
