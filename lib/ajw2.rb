ad9d9410e14233dfb2b02f883bf3558626a35fd9
dtan4 <dtanshi45@gmail.com>
2013-11-24 18:33:54 +0900

    Remove obsolute files

--- lib/ajw2.rb
+++ lib/ajw2.rb
@@ -1,12 +1,10 @@
 require "ajw2/version"
 require "ajw2/ajml"
-require "ajw2/generator/compiler"
+require "ajw2/generator"
 require "ajw2/model/application"
 require "ajw2/model/interfaces"
 require "ajw2/model/databases"
 require "ajw2/model/events"
-require "ajw2/template/html"
-require "ajw2/template/element"
 
 module Ajw2
   # Your code goes here...
--- lib/ajw2/generator/compiler.rb
+++
@@ -1,7 +0,0 @@
-module Ajw2
-  class Compiler
-    def generate_source(ajmlfile, out_dir)
-
-    end
-  end
-end
