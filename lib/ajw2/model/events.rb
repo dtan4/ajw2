4cfe3f23586c1e1f41050e58e54746e0651e9d95
dtan4 <dtanshi45@gmail.com>
2013-12-02 02:23:35 +0900

    Render event code in Ruby

--- lib/ajw2/model/events.rb
+++ lib/ajw2/model/events.rb
@@ -1,3 +1,5 @@
+require "active_support/inflector"
+
 module Ajw2::Model
   class Events
     attr_reader :source
@@ -5,6 +7,97 @@ module Ajw2::Model
     def initialize(source)
       raise ArgumentError, "Events section must be a Hash" unless source.class == Hash
       @source = source
+    end
+
+    def render_rb
+      raise Exception unless @source[:events]
+
+      @source[:events].inject([]) { |result, event| result << render_rb_event(event) }
+    end
+
+    private
+
+    def render_rb_event(event)
+      <<-EOS
+post "/#{event[:id]}" do
+  content_type :json
+  response = {}
+#{render_rb_params(event[:params], :ruby, 1)}
+#{render_rb_databases(event[:databases])}
+#{render_rb_interfaces(event[:interfaces])}
+  response.to_json
+end
+      EOS
+    end
+
+    def render_rb_params(params, type, indent)
+      params.inject([]) do |result, param|
+        result << "  " * indent + case type
+                                  when :hash
+                                    "#{param[:name]}: #{param[:name]}"
+                                  when :response
+                                    "response[:#{param[:name]}] = #{param[:name]}"
+                                  else
+                                    "#{param[:name]} = params[:#{param[:name]}]"
+                                  end
+      end.join("\n")
+    end
+
+    def render_rb_databases(databases)
+      databases.inject([]) do |result, database|
+        result << render_rb_database(database)
+      end.join("\n")
+    end
+
+    def render_rb_database(database)
+      case database[:func]
+      when "create" then render_rb_create(database)
+      when "read" then render_rb_read(database)
+      when "update" then render_rb_update(database)
+      when "delete" then render_rb_delete(database)
+      else
+        raise Exception
+      end
+    end
+
+    def render_rb_create(database)
+      <<-EOS.chomp
+  #{database[:id]} = #{database[:database].singularize.capitalize}.new(
+#{render_rb_params(database[:params], :hash, 2)}
+  )
+  #{database[:id]}.save
+      EOS
+    end
+
+    def render_rb_read(database)
+
+    end
+
+    def render_rb_update(database)
+
+    end
+
+    def render_rb_delete(database)
+
+    end
+
+    def render_rb_hash(params)
+
+    end
+
+    def render_rb_interfaces(interfaces)
+      interfaces.inject([]) do |result, interface|
+        result << render_rb_interface(interface)
+      end.join("\n")
+    end
+
+    def render_rb_interface(interface)
+      case interface[:func]
+      when "signup"
+      when "signin"
+      when "setValue"
+        render_rb_params(interface[:params], :response, 1)
+      end
     end
   end
 end
--- spec/ajw2/model/events_spec.rb
+++ spec/ajw2/model/events_spec.rb
@@ -2,7 +2,58 @@ require "spec_helper"
 
 module Ajw2::Model
   describe Events do
-    let(:source) {
+    let(:simple_source) {
+      {
+       events:
+       [
+        {
+         id: "event01",
+         target: "submitBtn",
+         type: "onClick",
+         realtime: false,
+         params: [
+                  {
+                   name: "message", type: "string",
+                   value: {
+                           element: "messageTextBox",
+                           func: "getValue",
+                           type: "element"
+                          }
+                  }
+                 ],
+         interfaces: [
+                      {
+                       id: "if01",
+                       element: "messageLabel",
+                       func: "setValue", type: "element",
+                       params: [
+                                {
+                                 name: "message",
+                                 type: "string"
+                                }
+                               ]
+
+                      }
+                     ],
+         databases: [
+                     {
+                      id: "db01",
+                      database: "messages",
+                      func: "create",
+                      params: [
+                               {
+                                name: "message",
+                                type: "string"
+                               }
+                              ]
+                     }
+                    ]
+        }
+       ]
+      }
+    }
+
+    let(:complex_source) {
       {
        events:
        [
@@ -56,7 +107,7 @@ module Ajw2::Model
 
     describe "#initialize" do
       context "with Hash" do
-        subject { Ajw2::Model::Events.new(source) }
+        subject { Ajw2::Model::Events.new(simple_source) }
         its(:source) { should be_instance_of Hash }
       end
 
@@ -66,6 +117,34 @@ module Ajw2::Model
             "Events section must be a Hash"
         end
       end
+    end
+
+    describe "#render_rb" do
+      context "with valid source" do
+        subject { Ajw2::Model::Events.new(simple_source).render_rb }
+        it { should be_an_instance_of Array }
+        it { should have(1).item }
+
+        it "should render the Sinatra source" do
+          expect(subject[0]).to eq(<<-EOS)
+post "/event01" do
+  content_type :json
+  response = {}
+  message = params[:message]
+  db01 = Message.new(
+    message: message
+  )
+  db01.save
+  response[:message] = message
+  response.to_json
+end
+                                   EOS
+        end
+      end
+    end
+
+    describe "#render_js" do
+
     end
   end
 end
