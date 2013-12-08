71e0b00091eb6ffb737a3a2c9b5c5314794a6e76
dtan4 <dtanshi45@gmail.com>
2013-12-08 16:35:14 +0900

    Load fixtures from external file

--- spec/ajw2/generator_spec.rb
+++ spec/ajw2/generator_spec.rb
@@ -8,6 +8,10 @@ module Ajw2
     end
 
     describe "#generate" do
+      before(:all) do
+        load File.expand_path("../../fixtures/generator_fixtures.rb", __FILE__)
+      end
+
       before do
         @application = double("application",
                               render_header: "title sample",
@@ -17,116 +21,19 @@ module Ajw2
                              render: "h1 sample")
 
         @databases = double("databases",
-                            render_migration: [
-                                               {
-                                                tablename: "users",
-                                                up: (<<-EOS),
-create_table :users do |t|
-  t.string :name
-  t.string :password
-end
-                                                EOS
-                                                down: "drop_table :users"
-                                               },
-                                               {
-                                                tablename: "messages",
-                                                up: (<<-EOS),
-create_table :messages do |t|
-  t.integer :user_id
-  t.string :message
-end
-                                                EOS
-                                                down: "drop_table :messages"
-                                               }
-                                              ],
-                            render_definition: [
-                                                (<<-EOS),
-class User < ActiveRecord::Base
-  validates_presence_of :name
-  validates_presence_of :password
-end
-EOS
-                                                (<<-EOS),
-class Message < ActiveRecord::Base
-  validates_presence_of :user_id
-  validates_presence_of :message
-end
-EOS
-                                               ])
+                            render_migration: RENDER_MIGRATION,
+                            render_definition: RENDER_DEFINITION)
 
         [:development, :test, :production].each do |env|
           @databases.stub(:render_config).with(env, @application).and_return("database: sample_#{env}")
         end
 
         @events = double("events",
-                         render_rb_ajax: [
-                                          (<<-EOS)
-post "/event01" do
-  content_type :json
-  response = {}
-  message = params[:message]
-  db01 = Message.new(
-    message: message
-  )
-  db01.save
-  response[:message] = message
-  response.to_json
-end
-EOS
-                                         ],
-                         render_rb_realtime: [
-                                          (<<-EOS)
-when "event01"
-  message = params[:message]
-  db01 = Message.new(
-    message: message
-  )
-  db01.save
-  response[:message] = message
-  EventMachine.next_tick do
-    settings.sockets.each { |s| s.send(response.to_json) }
-  end
-EOS
-                                             ],
-                         render_js_ajax: [
-                                          (<<-EOS)
-$('#submitBtn').click(function() {
-  var message = $('#messageTextBox').val();
-  $.ajax({
-    type: 'POST',
-    url: '/event01',
-    params: { 'message': message },
-    success: function(_xhr_msg) {
-      var _response = JSON.parse(_xhr_msg);
-      var if01 = _response['if01'];
-      $('#messageLabel').val(if01['message']);
-    },
-    error: function(_xhr, _xhr_msg) {
-      alert(_xhr_msg);
-    }
-  });
-});
-EOS
-                                         ],
-                         render_js_realtime: [
-                                              (<<-EOS)
-$('#submitBtn').click(function() {
-  var message = $('#messageTextBox').val();
-  var params = { 'message': message };
-  var request = { 'func': 'event01', 'params': params };
-  ws.send(JSON.stringfy(request));
-});
-EOS
-                                             ],
-                         render_js_onmessage: [
-                                               (<<-EOS)
-case 'event01':
-  var _response = _ws_json['msg'];
-  var if01 = _response['if01'];
-  $('#messageLabel').val(if01['message']);
-  break;
-EOS
-                                              ])
+                         render_rb_ajax: RENDER_RB_AJAX,
+                         render_rb_realtime: RENDER_RB_REALTIME,
+                         render_js_ajax: RENDER_JS_AJAX,
+                         render_js_realtime: RENDER_JS_REALTIME,
+                         render_js_onmessage: RENDER_JS_ONMESSAGE)
         @outdir = File.expand_path("../tmp", __FILE__)
         @generator =
           Ajw2::Generator.new(@application, @interfaces, @databases, @events)
@@ -161,149 +68,27 @@ EOS
         end
 
         it "should generate app.rb" do
-          expect(open(File.expand_path("app.rb", @outdir)).read).to eq <<-EOS
-class User < ActiveRecord::Base
-  validates_presence_of :name
-  validates_presence_of :password
-end
-
-class Message < ActiveRecord::Base
-  validates_presence_of :user_id
-  validates_presence_of :message
-end
-
-class App < Sinatra::Base
-  configure do
-    register Sinatra::ActiveRecordExtension
-    set :sockets, []
-    use Rack::Session::Cookie, expire_after: 3600, secret: "salt"
-    Slim::Engine.default_options[:pretty] = true
-  end
-
-  get "/" do
-    if !request.websocket?
-      slim :index
-    else
-      request.websocket do |ws|
-        ws.onopen do
-          settings.sockets << ws
-        end
-
-        ws.onmessage do |msg|
-          _ws_json = JSON.parse(msg)
-          response = {}
-          case _ws_json["func"]
-          when "event01"
-            message = params[:message]
-            db01 = Message.new(
-              message: message
-            )
-            db01.save
-            response[:message] = message
-            EventMachine.next_tick do
-              settings.sockets.each { |s| s.send(response.to_json) }
-            end
-          else
-          end
-        end
-
-        ws.onclose do
-          settings.sockets.delete(ws)
-        end
-      end
-    end
-  end
-
-  post "/event01" do
-    content_type :json
-    response = {}
-    message = params[:message]
-    db01 = Message.new(
-      message: message
-    )
-    db01.save
-    response[:message] = message
-    response.to_json
-  end
-end
-          EOS
+          expect(open(File.expand_path("app.rb", @outdir)).read).to eq APP_RB
         end
 
         it "should generate views/layout.slim" do
-          expect(open(File.expand_path("views/layout.slim", @outdir)).read).to eq <<-EOS
-doctype html
-html
-  head
-    title sample
-  html
-    == yield
-    script src="/js/jquery.min.js"
-          EOS
-        end
-
-        it "should generate views/layout.slim" do
-          expect(open(File.expand_path("views/layout.slim", @outdir)).read).to eq <<-EOS
-doctype html
-html
-  head
-    title sample
-  html
-    == yield
-    script src="/js/jquery.min.js"
-          EOS
+          expect(open(File.expand_path("views/layout.slim", @outdir)).read).to eq LAYOUT_SLIM
         end
 
         it "should generate views/index.slim" do
-          expect(open(File.expand_path("views/index.slim", @outdir)).read).to eq <<-EOS
-h1 sample
-          EOS
+          expect(open(File.expand_path("views/index.slim", @outdir)).read).to eq INDEX_SLIM
         end
 
         it "should generate config/database.yml" do
-          expect(open(File.expand_path("config/database.yml", @outdir)).read).to eq <<-EOS
-development:
-  database: sample_development
-
-test:
-  database: sample_test
-
-production:
-  database: sample_production
-          EOS
+          expect(open(File.expand_path("config/database.yml", @outdir)).read).to eq DATABASE_YML
         end
 
         it "should generate db/migrate/001_create_users.rb" do
-          expect(open(File.expand_path("db/migrate/001_create_users.rb", @outdir)).read).to eq <<-EOS
-class CreateUsers < ActiveRecord::Migration
-  def up
-    create_table :users do |t|
-      t.string :name
-      t.string :password
-    end
-  end
-
-  def down
-    drop_table :users
-  end
-end
-          EOS
+          expect(open(File.expand_path("db/migrate/001_create_users.rb", @outdir)).read).to eq CREATE_USERS_RB
         end
 
         it "should generate db/migrate/002_create_messages.rb" do
-          expect(open(File.expand_path("db/migrate/002_create_messages.rb", @outdir)).read).to eq <<-EOS
-class CreateMessages < ActiveRecord::Migration
-  def up
-    create_table :messages do |t|
-      t.integer :user_id
-      t.string :message
-    end
-  end
-
-  def down
-    drop_table :messages
-  end
-end
-          EOS
+          expect(open(File.expand_path("db/migrate/002_create_messages.rb", @outdir)).read).to eq CREATE_MESSAGES_RB
         end
       end
 
---
+++ spec/fixtures/generator_fixtures.rb
@@ -0,0 +1,232 @@
+RENDER_MIGRATION = [
+                    {
+                     tablename: "users",
+                     up: (<<-EOS),
+create_table :users do |t|
+  t.string :name
+  t.string :password
+end
+                                                EOS
+                     down: "drop_table :users"
+                    },
+                    {
+                     tablename: "messages",
+                     up: (<<-EOS),
+create_table :messages do |t|
+  t.integer :user_id
+  t.string :message
+end
+                                                EOS
+                     down: "drop_table :messages"
+                    }
+                   ]
+
+RENDER_DEFINITION = [
+                     (<<-EOS),
+class User < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :password
+end
+EOS
+                     (<<-EOS),
+class Message < ActiveRecord::Base
+  validates_presence_of :user_id
+  validates_presence_of :message
+end
+EOS
+                    ]
+
+RENDER_RB_AJAX = [
+                  (<<-EOS)
+post "/event01" do
+  content_type :json
+  response = {}
+  message = params[:message]
+  db01 = Message.new(
+    message: message
+  )
+  db01.save
+  response[:message] = message
+  response.to_json
+end
+EOS
+                 ]
+
+RENDER_RB_REALTIME = [
+                      (<<-EOS)
+when "event01"
+  message = params[:message]
+  db01 = Message.new(
+    message: message
+  )
+  db01.save
+  response[:message] = message
+  EventMachine.next_tick do
+    settings.sockets.each { |s| s.send(response.to_json) }
+  end
+EOS
+                     ]
+
+RENDER_JS_AJAX = [
+                  (<<-EOS)
+$('#submitBtn').click(function() {
+  var message = $('#messageTextBox').val();
+  $.ajax({
+    type: 'POST',
+    url: '/event01',
+    params: { 'message': message },
+    success: function(_xhr_msg) {
+      var _response = JSON.parse(_xhr_msg);
+      var if01 = _response['if01'];
+      $('#messageLabel').val(if01['message']);
+    },
+    error: function(_xhr, _xhr_msg) {
+      alert(_xhr_msg);
+    }
+  });
+});
+EOS
+                 ]
+
+RENDER_JS_REALTIME = [
+                      (<<-EOS)
+$('#submitBtn').click(function() {
+  var message = $('#messageTextBox').val();
+  var params = { 'message': message };
+  var request = { 'func': 'event01', 'params': params };
+  ws.send(JSON.stringfy(request));
+});
+EOS
+                     ]
+
+RENDER_JS_ONMESSAGE = [
+                       (<<-EOS)
+case 'event01':
+  var _response = _ws_json['msg'];
+  var if01 = _response['if01'];
+  $('#messageLabel').val(if01['message']);
+  break;
+EOS
+                      ]
+
+APP_RB = <<-EOS
+class User < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :password
+end
+
+class Message < ActiveRecord::Base
+  validates_presence_of :user_id
+  validates_presence_of :message
+end
+
+class App < Sinatra::Base
+  configure do
+    register Sinatra::ActiveRecordExtension
+    set :sockets, []
+    use Rack::Session::Cookie, expire_after: 3600, secret: "salt"
+    Slim::Engine.default_options[:pretty] = true
+  end
+
+  get "/" do
+    if !request.websocket?
+      slim :index
+    else
+      request.websocket do |ws|
+        ws.onopen do
+          settings.sockets << ws
+        end
+
+        ws.onmessage do |msg|
+          _ws_json = JSON.parse(msg)
+          response = {}
+          case _ws_json["func"]
+          when "event01"
+            message = params[:message]
+            db01 = Message.new(
+              message: message
+            )
+            db01.save
+            response[:message] = message
+            EventMachine.next_tick do
+              settings.sockets.each { |s| s.send(response.to_json) }
+            end
+          else
+          end
+        end
+
+        ws.onclose do
+          settings.sockets.delete(ws)
+        end
+      end
+    end
+  end
+
+  post "/event01" do
+    content_type :json
+    response = {}
+    message = params[:message]
+    db01 = Message.new(
+      message: message
+    )
+    db01.save
+    response[:message] = message
+    response.to_json
+  end
+end
+          EOS
+
+LAYOUT_SLIM = <<-EOS
+doctype html
+html
+  head
+    title sample
+  html
+    == yield
+    script src="/js/jquery.min.js"
+          EOS
+
+INDEX_SLIM = <<-EOS
+h1 sample
+          EOS
+
+DATABASE_YML = <<-EOS
+development:
+  database: sample_development
+
+test:
+  database: sample_test
+
+production:
+  database: sample_production
+          EOS
+
+CREATE_USERS_RB = <<-EOS
+class CreateUsers < ActiveRecord::Migration
+  def up
+    create_table :users do |t|
+      t.string :name
+      t.string :password
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+end
+          EOS
+
+CREATE_MESSAGES_RB = <<-EOS
+class CreateMessages < ActiveRecord::Migration
+  def up
+    create_table :messages do |t|
+      t.integer :user_id
+      t.string :message
+    end
+  end
+
+  def down
+    drop_table :messages
+  end
+end
+          EOS
