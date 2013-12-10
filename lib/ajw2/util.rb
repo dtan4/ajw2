d5e4a010e01fd81afd97ae66d05a5ce2d75a0aa7
dtan4 <dtanshi45@gmail.com>
2013-12-10 11:34:46 +0900

    Implement Ajw2::Util#valid_hash?

--- lib/ajw2/util.rb
+++ lib/ajw2/util.rb
@@ -6,4 +6,26 @@ module Ajw2::Util
       line = (line.strip == "") ? "" : "  " * depth + line
     end.join("").chomp
   end
+
+  def valid_hash?(hash, regexp = /\A[^'"]+\z/)
+    raise ArgumentError, "Non-Hash argument is given" unless hash.kind_of? Hash
+    valid_value?(hash, regexp)
+  end
+
+  private
+
+  def valid_value?(value, regexp)
+    case value
+    when String
+      return false unless value =~ regexp
+    when Hash
+      return false unless value.each_value.inject(true) { |res, val| res &= valid_value?(val, regexp) }
+    when Array
+      return false unless value.inject(true) { |res, val| res &= valid_value?(val, regexp) }
+    else
+
+    end
+
+    return true
+  end
 end
--- spec/ajw2/util_spec.rb
+++ spec/ajw2/util_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require "spec_helper"
 
 describe Ajw2::Util do
@@ -25,6 +26,56 @@ describe Ajw2::Util do
     context "with negative number" do
       it "should raise Exception" do
         expect { @dummy.indent("hoge", -2) }.to raise_error ArgumentError, "Negative number is given"
+      end
+    end
+  end
+
+  describe "#valid_value?" do
+    context "with valid Hash contains only ascii-char" do
+      it "should return true" do
+        expect(@dummy.valid_hash?({ hoge: "fuga", foo: "bar" })).to be_true
+      end
+    end
+
+    context "with valid Hash contains breakline" do
+      it "should return true" do
+        expect(@dummy.valid_hash?({ hoge: "fuga\npiyo", foo: "bar" })).to be_true
+      end
+    end
+
+    context "with valid Hash contains Unicode char" do
+      it "should return true" do
+        expect(@dummy.valid_hash?({ hoge: "ふが", foo: "ばー" })).to be_true
+      end
+    end
+
+    context "with valid deep Hash" do
+      it "should return true" do
+        expect(@dummy.valid_hash?({ hoge: "fuga", foo: { bar: "baz" } })).to be_true
+      end
+    end
+
+    context "with invalid hash" do
+      it "should return false" do
+        expect(@dummy.valid_hash?({ hoge: "fu'ga", foo: "bar" })).to be_false
+      end
+    end
+
+    context "with invalid deep hash" do
+      it "should return false" do
+        expect(@dummy.valid_hash?({ hoge: "fuga", foo: { bar: "ba'z" } })).to be_false
+      end
+    end
+
+    context "with regexp" do
+      it "should validate with given regexp" do
+        expect(@dummy.valid_hash?({ hoge: "ふが", foo: "ばー" }, /^[a-zA-Z0-9]+$/)).to be_false
+      end
+    end
+
+    context "with non-Hash" do
+      it "should raise Exception" do
+        expect { @dummy.valid_hash?("hoge") }.to raise_error ArgumentError, "Non-Hash argument is given"
       end
     end
   end
