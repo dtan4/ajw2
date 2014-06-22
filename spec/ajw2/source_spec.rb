21c30b8b459acf44e0610ca7fd785c1d7fde4eaf
dtan4 <dtanshi45@gmail.com>
2014-06-22 22:22:14 +0900

    Never use instance variable in source_spec.rb

--- spec/ajw2/source_spec.rb
+++ spec/ajw2/source_spec.rb
@@ -2,52 +2,64 @@ require "spec_helper"
 
 module Ajw2
   describe Source do
-    before(:all) do
-      @json_path = fixture_path("chat.json")
-      @yaml_path = fixture_path("chat.yaml")
-      @yml_path = fixture_path("chat.yml")
-      @invalid_path = fixture_path("chat.txt")
+    let(:json_path) do
+      fixture_path("chat.json")
+    end
+
+    let(:yaml_path) do
+      fixture_path("chat.yaml")
+    end
+
+    let(:yml_path) do
+      fixture_path("chat.yml")
+    end
+
+    let(:invalid_path) do
+      fixture_path("chat.txt")
+    end
+
+    let(:source) do
+      Ajw2::Source.new
     end
 
     describe "#parse_file" do
       shared_context "with valid file" do
         before do
-          @source = Ajw2::Source.new
-          @source.parse_file(path)
+          source.parse_file(path)
         end
 
         it "should set application" do
-          expect(@source.application).to be_an_instance_of Ajw2::Model::Application
+          expect(source.application).to be_an_instance_of Ajw2::Model::Application
         end
 
         it "should set interface" do
-          expect(@source.interface).to be_an_instance_of Ajw2::Model::Interface
+          expect(source.interface).to be_an_instance_of Ajw2::Model::Interface
         end
 
         it "should set database" do
-          expect(@source.database).to be_an_instance_of Ajw2::Model::Database
+          expect(source.database).to be_an_instance_of Ajw2::Model::Database
         end
 
         it "should set event" do
-          expect(@source.event).to be_an_instance_of Ajw2::Model::Event
+          expect(source.event).to be_an_instance_of Ajw2::Model::Event
         end
       end
 
       context "with .json" do
         include_context "with valid file" do
-          let(:path) { @json_path }
+          let(:path) { json_path }
         end
       end
 
       context "with .yaml" do
         include_context "with valid file" do
-          let(:path) { @yaml_path }
+          let(:path) { yaml_path }
         end
       end
 
       context "with .yml" do
         include_context "with valid file" do
-          let(:path) { @yml_path }
+          let(:path) { yml_path }
         end
       end
 
@@ -55,7 +67,7 @@ module Ajw2
         it "should raise Exception" do
           expect {
             source = Ajw2::Source.new
-            source.parse_file(@invalid_source_path)
+            source.parse_file(invalid_source_path)
           }.to raise_error
         end
       end
@@ -64,7 +76,7 @@ module Ajw2
         it "should raise Exception" do
           expect {
             source = Ajw2::Source.new
-            source.parse_file(@invalid_ext_path)
+            source.parse_file(invalid_ext_path)
           }.to raise_error
         end
       end
@@ -82,7 +94,7 @@ module Ajw2
         it "should raise Exception" do
           expect {
             source = Ajw2::Source.new
-            source.parse_file(@invalid_path + "hogehoge")
+            source.parse_file(invalid_path + "hogehoge")
           }.to raise_error
         end
       end
