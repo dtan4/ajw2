3573870359143fa049ae77a81dceb0cef2b32365
dtan4 <dtanshi45@gmail.com>
2014-06-22 22:30:32 +0900

    Never use instance variable in util_spec.rb

--- spec/ajw2/util_spec.rb
+++ spec/ajw2/util_spec.rb
@@ -3,16 +3,16 @@ require "spec_helper"
 
 describe Ajw2::Util do
   class DummyClass
+    include Ajw2::Util
   end
 
-  before(:all) do
-    @dummy = DummyClass.new
-    @dummy.extend Ajw2::Util
+  let(:dummy) do
+    DummyClass.new
   end
 
   describe "#indent" do
     context "with 0" do
-      subject { @dummy.indent("hoge", 0) }
+      subject { dummy.indent("hoge", 0) }
       it { is_expected.to be_kind_of String }
 
       it "should add no indent" do
@@ -21,7 +21,7 @@ describe Ajw2::Util do
     end
 
     context "with positive number" do
-      subject { @dummy.indent("hoge", 2) }
+      subject { dummy.indent("hoge", 2) }
       it { is_expected.to be_kind_of String }
 
       it "should add indent" do
@@ -31,7 +31,7 @@ describe Ajw2::Util do
 
     context "with negative number" do
       it "should raise Exception" do
-        expect { @dummy.indent("hoge", -2) }.to raise_error ArgumentError, "Negative number is given"
+        expect { dummy.indent("hoge", -2) }.to raise_error ArgumentError, "Negative number is given"
       end
     end
   end
@@ -39,58 +39,58 @@ describe Ajw2::Util do
   describe "#valid_hash?" do
     context "with valid Hash" do
       context "contains only ascii-char" do
-        subject { @dummy.valid_hash?({ hoge: "fuga", foo: "bar" }) }
+        subject { dummy.valid_hash?({ hoge: "fuga", foo: "bar" }) }
         it { is_expected.to be true }
       end
 
       context "contains breakline" do
-        subject { @dummy.valid_hash?({ hoge: "fuga\npiyo", foo: "bar" }) }
+        subject { dummy.valid_hash?({ hoge: "fuga\npiyo", foo: "bar" }) }
         it { is_expected.to be true }
       end
 
       context "contains Unicode char" do
-        subject { @dummy.valid_hash?({ hoge: "ふが", foo: "ばー" }) }
+        subject { dummy.valid_hash?({ hoge: "ふが", foo: "ばー" }) }
         it { is_expected.to be true }
       end
 
       context "contains Array" do
-        subject { @dummy.valid_hash?({ hoge: "fuga", foo: [{ foo: "bar" }, { baz: "moo" }] }) }
+        subject { dummy.valid_hash?({ hoge: "fuga", foo: [{ foo: "bar" }, { baz: "moo" }] }) }
         it { is_expected.to be true }
       end
 
       context "contains Hash" do
-        subject { @dummy.valid_hash?({ hoge: "fuga", foo: { bar: "baz" } }) }
+        subject { dummy.valid_hash?({ hoge: "fuga", foo: { bar: "baz" } }) }
         it { is_expected.to be true }
       end
     end
 
     context "with invalid hash" do
       context "contains only String" do
-        subject { @dummy.valid_hash?({ hoge: "fu'ga", foo: "bar" }) }
+        subject { dummy.valid_hash?({ hoge: "fu'ga", foo: "bar" }) }
         it { is_expected.to be false }
       end
 
       context "contains hash" do
-        subject { @dummy.valid_hash?({ hoge: "fuga", foo: { bar: "ba'z" } }) }
+        subject { dummy.valid_hash?({ hoge: "fuga", foo: { bar: "ba'z" } }) }
         it { is_expected.to be false }
       end
     end
 
     context "with regexp" do
-      subject { @dummy.valid_hash?({ hoge: "ふが", foo: "ばー" }, /^[a-zA-Z0-9]+$/) }
+      subject { dummy.valid_hash?({ hoge: "ふが", foo: "ばー" }, /^[a-zA-Z0-9]+$/) }
       it { is_expected.to be false }
     end
 
     context "with non-Hash" do
       it "should raise Exception" do
-        expect { @dummy.valid_hash?("hoge") }.to raise_error ArgumentError, "Non-Hash argument is given"
+        expect { dummy.valid_hash?("hoge") }.to raise_error ArgumentError, "Non-Hash argument is given"
       end
     end
   end
 
   describe "#symbolize_keys" do
     context "with flat Hash" do
-      subject { @dummy.symbolize_keys({ "hoge" => "fuga", "foo" => "bar" }) }
+      subject { dummy.symbolize_keys({ "hoge" => "fuga", "foo" => "bar" }) }
 
       it "shuold return Hash with symbolized keys" do
         expect(subject).to eq({ hoge: "fuga", foo: "bar" })
@@ -98,7 +98,7 @@ describe Ajw2::Util do
     end
 
     context "with deep Hash" do
-      subject { @dummy.symbolize_keys({ "hoge" => "fuga", "foo" => { "bar" => "baz" } }) }
+      subject { dummy.symbolize_keys({ "hoge" => "fuga", "foo" => { "bar" => "baz" } }) }
 
       it "should return Hash with symbolized keys" do
         expect(subject).to eq({ hoge: "fuga", foo: { bar: "baz" } })
